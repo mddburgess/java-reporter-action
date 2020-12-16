55bac1476de4c8ec753f89d10a4f38cbb65b1c41
Mike Burgess <mddburgess@users.noreply.github.com>
2020-12-15 19:52:16 -0800

    Add action.yml and dist.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log('Hello world!');
+import * as core from '@actions/core';
+import {checkSurefire} from './surefire';
+
+checkSurefire()
+    .finally(() => core.info('Surefire check finished.'));
