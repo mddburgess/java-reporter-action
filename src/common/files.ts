59ab2012be91cd72ed3375590eb5a3f44a7d3e59
Mike Burgess <mddburgess@users.noreply.github.com>
2020-12-15 19:42:05 -0800

    Add Surefire check.

--- src/common/files.ts
+++ src/common/files.ts
@@ -1,14 +1,12 @@
 import * as glob from '@actions/glob';
 
-export async function findFiles(searchPaths: string) {
-    const patterns = searchPaths.split(',');
-    const globber = await glob.create(patterns.join('\n'));
-    const paths = await globber.glob();
-    return paths;
+export async function findFiles(searchPaths: string[]) {
+    const globber = await glob.create(searchPaths.join('\n'));
+    return await globber.glob();
 }
 
 export async function findFile(searchPath: string) {
-    const paths = await findFiles(searchPath);
+    const paths = await findFiles([searchPath]);
     return paths[0];
 }
 
@@ -16,5 +14,5 @@ export async function findRelativePath(searchPath: string) {
     const globber = await glob.create(searchPath);
     const searchRoot = globber.getSearchPaths()[0];
     const absolutePaths = await globber.glob();
-    return absolutePaths[0].slice(searchRoot.length + 1);
+    return absolutePaths[0] && absolutePaths[0].slice(searchRoot.length + 1);
 }
--- src/common/tests/files.test.ts
+++ src/common/tests/files.test.ts
@@ -1,14 +1,14 @@
-import {findFile, findFiles} from '../files';
+import {findFile, findFiles, findRelativePath} from '../files';
 
 describe('findFiles()', () => {
 
     it('should return an array of found files', async () => {
-        const paths = await findFiles('src/**/*.ts');
+        const paths = await findFiles(['src/**/*.ts']);
         expect(paths.length).toBeGreaterThan(0);
     });
 
     it('should return an empty array when no files are found', async () => {
-        const paths = await findFiles('**/*.pdf');
+        const paths = await findFiles(['**/*.pdf']);
         expect(paths.length).toEqual(0);
     });
 });
@@ -25,3 +25,16 @@ describe('findFile()', () => {
         expect(path).toBeUndefined();
     })
 });
+
+describe('findRelativePath()', () => {
+
+    it('should return the relative path of a found file', async () => {
+        const path = await findRelativePath('**/SimpleTest.java');
+        expect(path).toEqual('example/src/test/java/org/example/SimpleTest.java');
+    });
+
+    it('should return undefined when no file is found', async () => {
+        const path = await findRelativePath('**/NotFound.java');
+        expect(path).toBeUndefined();
+    })
+})
