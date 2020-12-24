90ffbe0869e3d89f7bcecf814b00005c35574189
Mike Burgess <mddburgess@users.noreply.github.com>
2020-12-23 16:12:43 -0800

    Clear project.

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import * as core from "@actions/core";
+
+const main = async () => {
+  core.info("Hello world!");
+}
+
+main()
+  .catch(error => core.setFailed(error))
+  .finally(() => core.info("Java Reporter finished."));
