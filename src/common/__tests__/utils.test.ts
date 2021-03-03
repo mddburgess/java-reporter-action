839f9245a3b7745cda44371e61daa2621cd9380e
Mike Burgess <mddburgess@users.noreply.github.com>
2021-03-03 15:16:14 -0800

    Add unit tests for plural().

--- src/common/__tests__/utils.test.ts
+++ src/common/__tests__/utils.test.ts
@@ -1,4 +1,4 @@
-import { chunk } from "../utils";
+import { chunk, plural } from "../utils";
 
 describe("chunk()", () => {
   it("returns one empty chunk for undefined", () => {
@@ -24,5 +24,22 @@ describe("chunk()", () => {
   it("returns multiple chunks for an array greater than the chunk size", () => {
     const result = chunk([1, 2, 3], 2);
     expect(result).toStrictEqual([[1, 2], [3]]);
+  });
+});
+
+describe("plural()", () => {
+  it("uses plural form for quantity 0", () => {
+    const result = plural(0, "noun");
+    expect(result).toBe("0 nouns");
+  });
+
+  it("uses singular form for quantity 1", () => {
+    const result = plural(1, "noun");
+    expect(result).toBe("1 noun");
+  });
+
+  it("uses plural form for quantity greater than 1", () => {
+    const result = plural(2, "noun");
+    expect(result).toBe("2 nouns");
   });
 });
